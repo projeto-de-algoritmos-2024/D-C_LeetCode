92a0d058de7d83e76d86a803c58cda9963ecd3c3
pkbceira03 <cabeceira2003@gmail.com>
2024-12-18 14:36:23 -0300

    muda retorno

--- Minimum Number of Operations to Make String Sorted.py
+++ Minimum Number of Operations to Make String Sorted.py
@@ -1,8 +1,19 @@
-class MergeCount:
-    def merge(self, lista):
+class Solution:
+    def countSmaller(self, nums):
+        inversoes_comeco = {num: 0 for num in nums}
+    
+        lista_ordenada, inversoes = self.merge(nums, inversoes_comeco)
+
+        valores = list(inversoes.values())
+
+        return valores
+
+    def merge(self, lista, contador):
+
         #volta para quem chamou
         if len(lista)<=1:
-            return lista, 0
+            
+            return lista, contador
         
         #faz o meio da função como inteiro
         meio = len(lista)//2
@@ -10,49 +21,41 @@ class MergeCount:
         esquerda = lista[:meio]
         direita = lista[meio:]
 
-        esquerda_ordenada, inversoes_esquerda = self.merge(esquerda)
-        direita_ordenada, inversoes_direita = self.merge(direita)
+        esquerda_ordenada, contador = self.merge(esquerda, contador)
+        direita_ordenada, contador = self.merge(direita,contador)
 
         # junta a direita com a esquerda
-        lista_ordenada, num_inversoes = self.merge_and_conut(esquerda_ordenada, direita_ordenada)
+        lista_ordenada, contador = self.merge_and_conut(esquerda_ordenada, direita_ordenada, contador)
 
-        total_inversoes = inversoes_esquerda + num_inversoes + inversoes_direita
-        return lista_ordenada, total_inversoes
+        #total_inversoes = inversoes_esquerda + num_inversoes + inversoes_direita
+        return lista_ordenada, contador
 
-    def merge_and_conut(self, esquerda, direita):
+    def merge_and_conut(self, esquerda, direita, num_inversoes):
         i=0
         j=0
         ordenado = []
-        num_inversoes = 0
         while i < len(esquerda) and j < len(direita):
             if esquerda[i] <= direita[j]:
                 ordenado.append(esquerda[i])
-                i=i+1
+                i+=1
             elif direita[j] < esquerda[i]:
                 ordenado.append(direita[j])
-                j=j+1
-                num_inversoes=num_inversoes+len(esquerda)-i
-            
+                #como fazer para todos os da esquerda apartir do i
+                for num in esquerda[i:]:
+                    num_inversoes[num] += 1
+                
+                j+=1
+
         ordenado.extend(esquerda[i:])
         ordenado.extend(direita[j:])
-        
         return ordenado, num_inversoes
 
 
-entrada = input()
-
-lista_caracteres = list(entrada)
+entrada = [5,2,6,1]
     
-contador = MergeCount()
-    
-lista_ordenada, inversoes = contador.merge(lista_caracteres)
+contador = Solution()
 
-# Exibe os resultados
-#print("\nString original:", entrada)
-#print("String ordenada:", lista_ordenada)
-print(inversoes)
-    
+resultado = contador.countSmaller(entrada)
 
-
-
+print(resultado)
 
