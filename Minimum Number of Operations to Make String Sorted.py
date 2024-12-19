b02e64bebeda893df2142594047fc6faab17d948
Brunna Louise Martins <98557500+brunna-martins@users.noreply.github.com>
2024-12-19 08:55:39 -0300

    adiciona código submetido no leetcode
    esse rodou

--- Minimum Number of Operations to Make String Sorted.py
+++ Minimum Number of Operations to Make String Sorted.py
@@ -1,61 +1,53 @@
 class Solution:
     def countSmaller(self, nums):
-        inversoes_comeco = {num: 0 for num in nums}
-    
-        lista_ordenada, inversoes = self.merge(nums, inversoes_comeco)
-
-        valores = list(inversoes.values())
-
-        return valores
-
-    def merge(self, lista, contador):
-
-        #volta para quem chamou
-        if len(lista)<=1:
-            
-            return lista, contador
+        n = len(nums)
+        # Array para armazenar os resultados
+        counts = [0] * n
+        # Array de índices para acompanhar a posição original dos elementos
+        indices = list(range(n))
         
-        #faz o meio da função como inteiro
-        meio = len(lista)//2
+        # Função de merge sort modificada
+        def merge_sort(start, end):
+            if end - start <= 1:  # Caso base: um único elemento
+                return
+            mid = (start + end) // 2
+            merge_sort(start, mid)
+            merge_sort(mid, end)
+            merge(start, mid, end)
 
-        esquerda = lista[:meio]
-        direita = lista[meio:]
+        def merge(start, mid, end):
+            temp = []
+            left, right = start, mid
+            smaller_count = 0
+            
+            while left < mid and right < end:
+                if nums[indices[right]] < nums[indices[left]]:
+                    # O elemento da direita é menor
+                    temp.append(indices[right])
+                    smaller_count += 1
+                    right += 1
+                else:
+                    # O elemento da esquerda é menor ou igual
+                    temp.append(indices[left])
+                    counts[indices[left]] += smaller_count
+                    left += 1
+            
+            # Adiciona os elementos restantes
+            while left < mid:
+                temp.append(indices[left])
+                counts[indices[left]] += smaller_count
+                left += 1
+            while right < end:
+                temp.append(indices[right])
+                right += 1
+            
+            # Atualiza o array de índices
+            for i in range(len(temp)):
+                indices[start + i] = temp[i]
+        
+        # Chama o merge sort
+        merge_sort(0, n)
+        return counts
 
-        esquerda_ordenada, contador = self.merge(esquerda, contador)
-        direita_ordenada, contador = self.merge(direita,contador)
-
-        # junta a direita com a esquerda
-        lista_ordenada, contador = self.merge_and_conut(esquerda_ordenada, direita_ordenada, contador)
-
-        #total_inversoes = inversoes_esquerda + num_inversoes + inversoes_direita
-        return lista_ordenada, contador
-
-    def merge_and_conut(self, esquerda, direita, num_inversoes):
-        i=0
-        j=0
-        ordenado = []
-        while i < len(esquerda) and j < len(direita):
-            if esquerda[i] <= direita[j]:
-                ordenado.append(esquerda[i])
-                i+=1
-            elif direita[j] < esquerda[i]:
-                ordenado.append(direita[j])
-                #como fazer para todos os da esquerda apartir do i
-                for num in esquerda[i:]:
-                    num_inversoes[num] += 1
-                
-                j+=1
-
-        ordenado.extend(esquerda[i:])
-        ordenado.extend(direita[j:])
-        return ordenado, num_inversoes
-
-
-entrada = [5,2,6,1]
-    
-contador = Solution()
-
-resultado = contador.countSmaller(entrada)
-
-print(resultado)
-
+# Testando os exemplos
+sol = Solution()
